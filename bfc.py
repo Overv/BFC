acbb942dbdef0a5814e2d72e593c69d071f573af
Alexander Overvoorde <overv161@gmail.com>
2013-08-28 05:51:48 +0200

    Add linker code for 32-bit ELF

--- bfc.py
+++ bfc.py
@@ -1,4 +1,6 @@
 import sys
+import os
+import struct
 
 #
 # Types of lexical tokens
@@ -93,7 +95,7 @@ class Parser:
     # Reads the next token
     def read(self):
         tok = self.peek()
-        self.index = self.index + 1
+        self.index += 1
         return tok
 
     # Reads the next token and checks if it's of the expected type
@@ -154,6 +156,68 @@ class Parser:
         return LoopNode(nodes)
 
 #
+# Linker
+#
+
+class Linker:
+    # Byte size of ELF file structures (ELF header and program header)
+    ELF_HDR_SIZE = 52
+    PRO_HDR_SIZE = 32
+    LOAD_ADDRESS = 0x08048000
+
+    def __init__(self, code):
+        self.code = code
+
+    # Write 32-bit ELF executable with generated machine code
+    def write(self, filename):
+        with open(filename, 'w') as f:
+            self.write_header(f)
+            self.write_program_header(f, len(self.code))
+            f.write(code)
+
+    # Write ELF header
+    # http://www.sco.com/developers/gabi/1998-04-29/ch4.eheader.html
+    def write_header(self, f):
+        entry_point = self.LOAD_ADDRESS + self.ELF_HDR_SIZE + self.PRO_HDR_SIZE
+
+        f.write('\x7FELF') # File identifier
+        f.write('\x01') # 32-bit
+        f.write('\x01') # Little-endian (x86)
+        f.write('\x00') # Current header version
+        f.write('\x00') # Unix System V ABI
+        f.write('\x00')
+        f.write('\x00' * 7) # Padding to 16 bytes
+
+        f.write(struct.pack('<H', 2)) # Executable file
+        f.write(struct.pack('<H', 3)) # Intel 80386
+        f.write(struct.pack('<I', 1)) # Current object file version
+        f.write(struct.pack('<I', entry_point)) # Entry point
+        f.write(struct.pack('<I', self.ELF_HDR_SIZE)) # Program header offset
+        f.write(struct.pack('<I', 0)) # Section header offset (none)
+        f.write(struct.pack('<I', 0)) # Processor flags (none)
+        f.write(struct.pack('<H', self.ELF_HDR_SIZE)) # ELF header size
+        f.write(struct.pack('<H', self.PRO_HDR_SIZE)) # Program header size
+        f.write(struct.pack('<H', 1)) # Program header table entries (1)
+        f.write(struct.pack('<H', 0)) # TODO: Section header size (none)
+        f.write(struct.pack('<H', 0)) # Section header table entries (none)
+        f.write(struct.pack('<H', 0)) # String table entry index (none)
+
+    # Write single program header entry
+    # It is common to simply load the entire executable into memory.
+    # http://www.sco.com/developers/gabi/1998-04-29/ch5.pheader.html
+    def write_program_header(self, f, code_size):
+        file_size = self.ELF_HDR_SIZE + self.PRO_HDR_SIZE + code_size
+
+        f.write(struct.pack('<I', 1)) # Load section into memory
+        f.write(struct.pack('<I', 0)) # File offset
+        f.write(struct.pack('<I', self.LOAD_ADDRESS)) # Virtual memory address
+        f.write(struct.pack('<I', self.LOAD_ADDRESS)) # Physical memory address
+        f.write(struct.pack('<I', file_size)) # File image size
+        f.write(struct.pack('<I', file_size)) # Memory image size
+        f.write(struct.pack('<I', 0x1 | 0x4)) # Execute and read flags
+        f.write(struct.pack('<I', 0x1000)) # Alignment (4 KB)
+
+#
 # Main program
 #
 
@@ -177,3 +241,11 @@ if __name__ == '__main__':
     except ParseException as e:
         sys.stderr.write('err: ' + str(e) + '\n')
         sys.exit(1)
+
+    # Generate code
+    # TODO
+    code = '\xB8\x01\x00\x00\x00\xBB\x2A\x00\x00\x00\xCD\x80'
+
+    # Write executable
+    executable_name = os.path.splitext(sys.argv[1])[0]
+    Linker(code).write(executable_name)
